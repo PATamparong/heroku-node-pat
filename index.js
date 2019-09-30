1b4b6713b4d6732eaee5bac2e16bdd8046d76f82
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-09-30 21:47:27 +0800

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
-var io = require('../..index.js')(server);
+var io = require('../..index')(server);
 var port = process.env.PORT || 3000;
 
 server.listen(port, () => {
